15e14670acbe750aa39c3909202a1410f62aa75c
Vytenis Dambrauskas <vytenisdambrauskas@gmail.com>
2023-03-24 14:30:36 +0200

    End. Needs better user interface and upgrades.

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import random
-#cards = [11,11,11,1,1,1,1,1,1,11,11,11]
+import os
+
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+    
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-hand = []
-computer_hand = []
-#prideti tuzo keitima i 1 jei iskrenta abu tuzai
-#first draw both players 2 cards
+
 def blackjack():
+    #First step. Both player and computer draws 2 cards each.
     hand = []
     computer_hand = []
     play_again = 'n'
@@ -16,26 +18,61 @@ def blackjack():
     computer_hand.append(cards[first_card])
     computer_hand.append(cards[second_card])
     computer_points += int(cards[first_card])
+    
+    # If both first cards are aces, second ace value equals to 1.
     if first_card == 11 and second_card == 11:
         computer_points += 1
     else:
         computer_points += int(cards[second_card])
+        
     first_card1 = cards.index(random.choice(cards))
     second_card1 = cards.index(random.choice(cards))
     hand.append(cards[first_card1])
     hand.append(cards[second_card1])
     player_points += int(cards[first_card1])
+    
+    # If both first cards are aces, second ace value equals to 1.
     if first_card1 == 11 and second_card1 == 11:
         player_points += 1
     else:
         player_points += int(cards[second_card1])
-    print('pirmas')
-    print(hand)
-    print(player_points)
-    print(f' computer has {computer_hand}')
-    print(computer_points)
+        
+    print(f"{player_name}'s hand: {hand}")
+    print(f' Computer has: {computer_hand[0]}, _')
     
-    #check if less than 17 points
+    #Asking player if he/she wants to draw another card.
+    should_continue = True
+    while should_continue:
+        draw_card = input('Would you like another card? y/n ').lower()
+        if draw_card == 'y' and player_points < 21:
+            extra_card = cards.index(random.choice(cards))
+            indexed_extra_card = cards[extra_card]
+            #If drawn card is Ace and sum of points > 21 than ace value becomes 1.
+            if player_points > 10 and indexed_extra_card == 11:
+                hand.append(cards[extra_card])
+                player_points += 1      
+                print(f' Your {hand}')
+            # if not ace stays - 11.
+            elif player_points <= 10 and indexed_extra_card == 11: 
+                hand.append(cards[extra_card])
+                player_points += int(hand[-1])       
+                print(f' Your hand: {hand}')
+            # any other card has its own unchanged value.
+            else: 
+                hand.append(cards[extra_card])
+                player_points += int(hand[-1])       
+                print(f' Your hand: {hand}')
+                if player_points > 21:
+                    should_continue = False
+                    print(f' Your hand: {hand}')
+                    print('You got bust!') 
+                    play_again = input(f"Hey {player_name}, would you like to play again?")
+                    cls()
+                else: 
+                    should_continue = True          
+        else:
+            should_continue = False
+    #Checks if computer has less than 17 points if so draws another card.
     while 17 > computer_points:
         another_card = cards.index(random.choice(cards))
         indexed_another_card = cards[another_card]
@@ -44,67 +81,36 @@ def blackjack():
             computer_points += 1
         else:
             computer_points += int(computer_hand[-1])
-
-        #ace if sum < 21, 11 = 11, if sum > 21 11 = 1
-        #placing..........................................
-        #if computer_points < 21 and indexed_another_card == 11:
-            #another_card = 11
-        #else:
-            #another_card = 1
-        #..................................................
-
-    #another card player
-    should_continue = True
-    while should_continue:
-        draw_card = input('Would you like another card? y/n ').lower()
-        if draw_card == 'y' and player_points < 21:
-            extra_card = cards.index(random.choice(cards))
-            indexed_extra_card = cards[extra_card]
-            if player_points > 10 and indexed_extra_card == 11:
-                hand.append(cards[extra_card])
-                player_points += 1      
-                print(hand)
-                print(f'sitas {player_points}')
-            elif player_points <= 10 and indexed_extra_card == 11: 
-                hand.append(cards[extra_card])
-                player_points += int(hand[-1])       
-                print(hand)
-                print(f'sitas {player_points}')
-            else: 
-                hand.append(cards[extra_card])
-                player_points += int(hand[-1])       
-                print(hand)
-                print(f'sitas {player_points}')
-                if player_points > 21:
-                    should_continue = False
-                    print(player_points)
-                    print(hand)
-                    print(computer_points)
-                    print(computer_hand)
-                    print('player bust') 
-                    play_again = input("Would you like to play again?")                 
-        else:
-            should_continue = False
-            if computer_points > 21:
-                print(f'player win, computer bust {computer_points}')
-                play_again = input("Would you like to play again?")
-            elif computer_points > player_points:
-                print('computer wins!')
-                print(computer_hand)
-                print(computer_points)
-                play_again = input("Would you like to play again?")
-            else:
-                print(hand)
-                print(player_points)
-                print(computer_hand)
-                print(computer_points)
-                print('player wwins!')        
-                play_again = input("Would you like to play again?")
+    #Calculating a winner.
+    if computer_points > 21:
+        print(f'{player_name} wins! computer bust.')
+        print(computer_hand)
+        play_again = input(f"Hey {player_name}, would you like to play again?")
+        cls()
+    elif computer_points > player_points:
+        print(f'Sorry {player_name} you lost. Computer wins!')
+        print(f' Your hand: {hand}')
+        print(f"Computer's hand: {computer_hand}")
+        play_again = input(f"Hey {player_name}, would you like to play again?")
+        cls()
+    else:
+        print(f' Your hand: {hand}')
+        print(f"Computer's hand: {computer_hand}")
+        print(f'{player_name} wins!')        
+        play_again = input(f"Hey {player_name}, would you like to play again?")
+        cls()
+    #Play one more time.            
     if play_again == 'y':
         hand = []
         computer_hand = []
         blackjack()
-    elif play_again == 'n':
-        print('bye')
-blackjack()
+    else:
+        print('See you later!')
+#Start of the game.        
+play_game = input('Would you like to play a game of blackjack? Type "y" if so.\n').lower()
+if play_game == "y":
+    player_name = input('Please enter your name: ')
+    blackjack()
+else: 
+    print('Bye!')
 
